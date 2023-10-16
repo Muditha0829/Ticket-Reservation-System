6c72e90e1dfdf981455f35809239a07cbd52d38f
Pasindu Perera <pasinduperera9910@gmail.com>
2023-10-16 06:53:02 +0530

    commit

--- web_application/src/SetProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use('/api', createProxyMiddleware({
-    target: 'http://pasinduperera-001-site1.atempurl.com',
-    changeOrigin: true,
-  }));
-};
